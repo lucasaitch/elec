5d80e3e475e95d166cca31195f855ed7a4cb4efe
interz21 <dcar7701@uni.sydney.edu.au>
2015-10-22 07:36:38 +0000

    better interface

--- db/migrate/20151021120245_drop_users_degrees_table.rb
+++
@@ -1,9 +0,0 @@
-class DropUsersDegreesTable < ActiveRecord::Migration
-  def up
-      drop_table :users_degrees
-    end
-
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
-  end
