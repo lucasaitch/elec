b0c6403fca399f91302e856c62f7e787ba69c882
sfer3453 <sfer3453@uni.sydney.edu.au>
2015-10-23 09:12:20 +1100

    modified category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
 
   has_and_belongs_to_many :listings, :join_table => "categories_listings"
   belongs_to :parent, :class_name => "Category", :foreign_key => "parent_category_id"
-  has_many :children, :class_name => "Category", :foreign_key => "parent_category_id"
+  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_category_id"
 
 end
