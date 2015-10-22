85155638ba9e42fb8c4926f2d1f0454569f82948
Lucas Hakewill <lhak7764@uni.sydney.edu.au>
2015-10-22 21:06:01 +1100

    lucas

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,12 +1,11 @@
 class UserProfilesController < ApplicationController
-
+  before_action :confirm_logged_in
   def show
     @session_user = User.find(session[:user_id])
      @avatar = @session_user.avatar.url(:thumb)
      @name = @session_user.first_name + " " + @session_user.last_name
      @units = UnitOfStudy.includes(:users).where('users.id' => session[:user_id])
   end
-
 
 
   def edit
